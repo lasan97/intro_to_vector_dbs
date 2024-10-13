1a6b121e3903227cf4584926c6f4063d49611341
Martin <martin@niceday.io>
2024-10-13 22:06:56 +0900

    import

---
+++ ingestion.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+
+
+
+if __name__ == "__main__":
+    print("ingesting...")
