d08a727cda9ed7e2470258a0ad481a67ff1e557b
Martin <martin@niceday.io>
2024-10-14 17:01:27 +0900

    using to local vectorstore

--- vectorstor.py
+++ vectorstor.py
@@ -1,19 +1,27 @@
 from dotenv import load_dotenv
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAI, OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 
 load_dotenv()
 
 if __name__ == '__main__':
     print("hi")
-    pdf_path = "/Users/lasanhan/workspace/python/ai/intro-to-vector-dbs/2210.03629v3.pdf"
+    pdf_path = "./2210.03629v3.pdf"
     loader = PyPDFLoader(file_path=pdf_path)
     document = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
     docs = text_splitter.split_documents(documents=document)
 
     embeddings = OpenAIEmbeddings()
-    vectorstore = FAISS.from_documents(docs, embeddings)
-    vectorstore.save_local("faiss_index_react")
+    # vectorstore = FAISS.from_documents(docs, embeddings)
+    # vectorstore.save_local("faiss_index_react")
+
+    new_vectorstore = FAISS.load_local("faiss_index_react", embeddings, allow_dangerous_deserialization=True)
+    qa = RetrievalQA.from_chain_type(llm=OpenAI(
+        model_name="gpt-3.5-turbo-instruct"
+    ), chain_type="stuff", retriever = new_vectorstore.as_retriever())
+    res = qa.run("Give me the gist of ReAct in 3 sentences")
+    print(res)
