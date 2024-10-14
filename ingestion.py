f341f6f50e7bad5b8460f31b8903cd2a8c8fc572
Martin <martin@niceday.io>
2024-10-14 13:55:43 +0900

    insert to vectorDB

--- ingestion.py
+++ ingestion.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
@@ -11,3 +13,16 @@ load_dotenv()
 
 if __name__ == "__main__":
     print("ingesting...")
+    loader = TextLoader("./mediumblog1.txt")
+    document = loader.load()
+
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+    print("ingesting...")
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ['INDEX_NAME'])
+    print("finish")
